22f766c00c91541b7f014643ebc366c8d55fb7fc
Graham Mendick <graham.mendick@gmail.com>
2016-04-03 15:35:29 +0100

    Updated to version 2.0.0

--- navigation.knockout.js
+++ navigation.knockout.js
@@ -1,38 +1,36 @@
 /**
- * Navigation v1.2.0
- * (c) Graham Mendick - http://grahammendick.github.io/navigation/example/knockout/navigation.html
- * License: Apache License 2.0
+ * Navigation Knockout v2.0.0
+ * (c) Graham Mendick - http://grahammendick.github.io/navigation/
+ * License: Apache-2.0
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.NavigationKnockout = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 (function (global){
-/// <reference path="navigation.d.ts" />
-/// <reference path="knockout.d.ts" />
-var Navigation = (typeof window !== "undefined" ? window['Navigation'] : typeof global !== "undefined" ? global['Navigation'] : null);
+"use strict";
 var ko = (typeof window !== "undefined" ? window['ko'] : typeof global !== "undefined" ? global['ko'] : null);
 var LinkUtility = (function () {
     function LinkUtility() {
     }
-    LinkUtility.setLink = function (element, linkAccessor) {
+    LinkUtility.setLink = function (stateNavigator, element, linkAccessor) {
         try {
-            element.href = Navigation.settings.historyManager.getHref(linkAccessor());
+            element.href = stateNavigator.historyManager.getHref(linkAccessor());
         }
         catch (e) {
             element.removeAttribute('href');
         }
     };
-    LinkUtility.getData = function (toData, includeCurrentData, currentDataKeys) {
+    LinkUtility.getData = function (stateNavigator, navigationData, includeCurrentData, currentDataKeys) {
         if (currentDataKeys)
-            toData = Navigation.StateContext.includeCurrentData(toData, currentDataKeys.trim().split(/\s*,\s*/));
+            navigationData = stateNavigator.stateContext.includeCurrentData(navigationData, currentDataKeys.trim().split(/\s*,\s*/));
         if (includeCurrentData)
-            toData = Navigation.StateContext.includeCurrentData(toData);
-        return toData;
+            navigationData = stateNavigator.stateContext.includeCurrentData(navigationData);
+        return navigationData;
     };
-    LinkUtility.isActive = function (key, val) {
-        if (!Navigation.StateContext.state)
+    LinkUtility.isActive = function (stateNavigator, key, val) {
+        if (!stateNavigator.stateContext.state)
             return false;
         if (val != null) {
-            var trackTypes = Navigation.StateContext.state.trackTypes;
-            var currentVal = Navigation.StateContext.data[key];
+            var trackTypes = stateNavigator.stateContext.state.trackTypes;
+            var currentVal = stateNavigator.stateContext.data[key];
             if (currentVal != null)
                 return trackTypes ? val === currentVal : val.toString() == currentVal.toString();
             else
@@ -48,29 +46,27 @@ var LinkUtility = (function () {
     LinkUtility.addListeners = function (element, setLink, allBindings, viewModel) {
         var _this = this;
         var lazy = !!allBindings.get('lazy');
+        var stateNavigator = allBindings.get('stateNavigator');
         ko.utils.registerEventHandler(element, 'click', function (e) {
             if (lazy)
                 setLink();
             if (!e.ctrlKey && !e.shiftKey && !e.metaKey && !e.altKey && !e.button) {
                 if (element.href) {
-                    var link = Navigation.settings.historyManager.getUrl(element);
+                    var link = stateNavigator.historyManager.getUrl(element);
                     var navigating = _this.getNavigating(allBindings, viewModel);
                     if (navigating(e, link)) {
                         if (e.preventDefault)
                             e.preventDefault();
                         else
                             e['returnValue'] = false;
-                        var historyAction = ko.unwrap(allBindings.get('historyAction'));
-                        if (typeof historyAction === 'string')
-                            historyAction = Navigation.HistoryAction[historyAction];
-                        Navigation.StateController.navigateLink(link, false, historyAction);
+                        stateNavigator.navigateLink(link, ko.unwrap(allBindings.get('historyAction')));
                     }
                 }
             }
         });
         if (!lazy) {
-            Navigation.StateController.onNavigate(setLink);
-            ko.utils.domNodeDisposal.addDisposeCallback(element, function () { return Navigation.StateController.offNavigate(setLink); });
+            stateNavigator.onNavigate(setLink);
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function () { return stateNavigator.offNavigate(setLink); });
         }
         else {
             ko.utils.registerEventHandler(element, 'mousedown', function (e) { return setLink(); });
@@ -86,28 +82,30 @@ var LinkUtility = (function () {
         };
     };
     return LinkUtility;
-})();
+}());
 module.exports = LinkUtility;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(_dereq_,module,exports){
 (function (global){
+"use strict";
 var LinkUtility = _dereq_('./LinkUtility');
-var Navigation = (typeof window !== "undefined" ? window['Navigation'] : typeof global !== "undefined" ? global['Navigation'] : null);
 var ko = (typeof window !== "undefined" ? window['ko'] : typeof global !== "undefined" ? global['ko'] : null);
 var NavigationBackLink = ko.bindingHandlers['navigationBackLink'] = {
     init: function (element, valueAccessor, allBindings, viewModel) {
-        LinkUtility.addListeners(element, function () { return setNavigationBackLink(element, valueAccessor); }, allBindings, viewModel);
+        LinkUtility.addListeners(element, function () { return setNavigationBackLink(element, valueAccessor, allBindings); }, allBindings, viewModel);
     },
-    update: function (element, valueAccessor) {
-        setNavigationBackLink(element, valueAccessor);
+    update: function (element, valueAccessor, allBindings) {
+        setNavigationBackLink(element, valueAccessor, allBindings);
     }
 };
-function setNavigationBackLink(element, valueAccessor) {
-    LinkUtility.setLink(element, function () { return Navigation.StateController.getNavigationBackLink(ko.unwrap(valueAccessor())); });
+function setNavigationBackLink(element, valueAccessor, allBindings) {
+    var stateNavigator = allBindings.get('stateNavigator');
+    LinkUtility.setLink(stateNavigator, element, function () { return stateNavigator.getNavigationBackLink(ko.unwrap(valueAccessor())); });
 }
 module.exports = NavigationBackLink;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./LinkUtility":1}],3:[function(_dereq_,module,exports){
+"use strict";
 var NavigationBackLink = _dereq_('./NavigationBackLink');
 var NavigationLink = _dereq_('./NavigationLink');
 var RefreshLink = _dereq_('./RefreshLink');
@@ -118,12 +116,12 @@ var NavigationKnockout = (function () {
     NavigationKnockout.NavigationLink = NavigationLink;
     NavigationKnockout.RefreshLink = RefreshLink;
     return NavigationKnockout;
-})();
+}());
 module.exports = NavigationKnockout;
 },{"./NavigationBackLink":2,"./NavigationLink":4,"./RefreshLink":5}],4:[function(_dereq_,module,exports){
 (function (global){
+"use strict";
 var LinkUtility = _dereq_('./LinkUtility');
-var Navigation = (typeof window !== "undefined" ? window['Navigation'] : typeof global !== "undefined" ? global['Navigation'] : null);
 var ko = (typeof window !== "undefined" ? window['ko'] : typeof global !== "undefined" ? global['ko'] : null);
 var NavigationLink = ko.bindingHandlers['navigationLink'] = {
     init: function (element, valueAccessor, allBindings, viewModel) {
@@ -134,29 +132,26 @@ var NavigationLink = ko.bindingHandlers['navigationLink'] = {
     }
 };
 function setNavigationLink(element, valueAccessor, allBindings) {
-    var action = ko.unwrap(valueAccessor());
+    var stateKey = ko.unwrap(valueAccessor());
     var data = {};
-    var toData = ko.unwrap(allBindings.get('toData'));
+    var navigationData = ko.unwrap(allBindings.get('navigationData'));
     var active = true;
-    for (var key in toData) {
-        var val = ko.unwrap(toData[key]);
+    var stateNavigator = allBindings.get('stateNavigator');
+    for (var key in navigationData) {
+        var val = ko.unwrap(navigationData[key]);
         data[key] = val;
-        active = active && LinkUtility.isActive(key, val);
+        active = active && LinkUtility.isActive(stateNavigator, key, val);
     }
-    LinkUtility.setLink(element, function () { return Navigation.StateController.getNavigationLink(action, LinkUtility.getData(data, ko.unwrap(allBindings.get('includeCurrentData')), ko.unwrap(allBindings.get('currentDataKeys')))); });
-    active = active && !!element.href && isActive(action);
+    LinkUtility.setLink(stateNavigator, element, function () { return stateNavigator.getNavigationLink(stateKey, LinkUtility.getData(stateNavigator, data, ko.unwrap(allBindings.get('includeCurrentData')), ko.unwrap(allBindings.get('currentDataKeys')))); });
+    active = active && !!element.href && stateNavigator.stateContext.state && stateNavigator.stateContext.state.key === stateKey;
     LinkUtility.setActive(element, active, ko.unwrap(allBindings.get('activeCssClass')), ko.unwrap(allBindings.get('disableActive')));
-}
-function isActive(action) {
-    var nextState = Navigation.StateController.getNextState(action);
-    return nextState === nextState.parent.initial && nextState.parent === Navigation.StateContext.dialog;
 }
 module.exports = NavigationLink;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./LinkUtility":1}],5:[function(_dereq_,module,exports){
 (function (global){
+"use strict";
 var LinkUtility = _dereq_('./LinkUtility');
-var Navigation = (typeof window !== "undefined" ? window['Navigation'] : typeof global !== "undefined" ? global['Navigation'] : null);
 var ko = (typeof window !== "undefined" ? window['ko'] : typeof global !== "undefined" ? global['ko'] : null);
 var RefreshLink = ko.bindingHandlers['refreshLink'] = {
     init: function (element, valueAccessor, allBindings, viewModel) {
@@ -168,14 +163,15 @@ var RefreshLink = ko.bindingHandlers['refreshLink'] = {
 };
 function setRefreshLink(element, valueAccessor, allBindings) {
     var data = {};
-    var toData = ko.unwrap(valueAccessor());
+    var navigationData = ko.unwrap(valueAccessor());
     var active = true;
-    for (var key in toData) {
-        var val = ko.unwrap(toData[key]);
+    var stateNavigator = allBindings.get('stateNavigator');
+    for (var key in navigationData) {
+        var val = ko.unwrap(navigationData[key]);
         data[key] = val;
-        active = active && LinkUtility.isActive(key, val);
+        active = active && LinkUtility.isActive(stateNavigator, key, val);
     }
-    LinkUtility.setLink(element, function () { return Navigation.StateController.getRefreshLink(LinkUtility.getData(data, ko.unwrap(allBindings.get('includeCurrentData')), ko.unwrap(allBindings.get('currentDataKeys')))); });
+    LinkUtility.setLink(stateNavigator, element, function () { return stateNavigator.getRefreshLink(LinkUtility.getData(stateNavigator, data, ko.unwrap(allBindings.get('includeCurrentData')), ko.unwrap(allBindings.get('currentDataKeys')))); });
     active = active && !!element.href;
     LinkUtility.setActive(element, active, ko.unwrap(allBindings.get('activeCssClass')), ko.unwrap(allBindings.get('disableActive')));
 }
